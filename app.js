d9dcf5aa222acb6a4ea633f91421c23bb72c44d7
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-09-23 18:55:02 +0900

    채팅

---
+++ app.js
@@ -0,0 +1,111 @@
+// @ts-check
+const Koa = require('koa');
+const websockify = require('koa-websocket');
+const route = require('koa-route');
+const serve = require('koa-static');
+const mount = require('koa-mount');
+
+const Pug = require('koa-pug');
+const path = require('path');
+
+const MongoClient = require('./public/mongo');
+
+const _client = MongoClient.connect();
+
+const app = websockify(new Koa());
+const PORT = 4500;
+
+app.use(mount('/public', serve('public')));
+
+const pug = new Pug({
+  viewPath: path.resolve(__dirname, './views'),
+  app,
+});
+
+app.ws.use(
+  route.all('/chat', async (ctx) => {
+    const { server } = app.ws;
+
+    const client = await _client;
+    const cursor = client.db('kdt1').collection('chats');
+    const chats = cursor.find({}, {
+      sort: {
+        createdAt: 1,
+      }
+    });
+    const chatsData = await chats.toArray();
+
+    ctx.websocket.send(
+      JSON.stringify({
+        type: 'sync',
+        data: {
+          chatsData,
+        },
+      })
+    );
+
+    server.clients.forEach((client) => {
+      client.send(
+        JSON.stringify({
+          type: 'chat',
+          data: {
+            name: '서버',
+            msg: `새로운 유저가 참여했습니다. 현재 유저 수 ${server?.clients.size}`,
+            bg: 'bg-danger',
+            text: 'text-white',
+          },
+        })
+      );
+    });
+
+    ctx.websocket.on('message', async (message) => {
+      const chat = JSON.parse(message);
+
+      const insertClient = await _client;
+      const chatCursor = insertClient.db('kdt1').collection('chats');
+      await chatCursor.insertOne({
+        // name: chat.name,
+        // msg: chat.msg,
+        // bg: chat.bg,
+        // text: chat.text,
+        ...chat,
+        createdAt: new Date(),
+      });
+
+      server?.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: 'chat',
+            data: {
+              ...chat,
+            },
+          })
+        );
+      });
+    });
+
+    ctx.websocket.on('close', () => {
+      server.clients.forEach((client) => {
+        client.send(
+          JSON.stringify({
+            type: 'chat',
+            data: {
+              name: '서버',
+              msg: `유저 1명이 나갔습니다. 현재 유저 수 ${server?.clients.size}`,
+              bg: 'bg-dark',
+              text: 'text-white',
+            },
+          })
+        );
+      });
+    });
+  })
+);
+
+app.use(async (ctx, next) => {
+  await ctx.render('chat');
+});
+
+app.listen(PORT, () => {
+  console.log(`서버 ${PORT}에서 작동 중입니다`);
+});
